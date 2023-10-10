671f846fe912570f019472683e5b3c9e8aaca8bf
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-10-10 17:31:35 +0530

    Added Complete All button functionality

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const addBtn = document.getElementById("add-btn");
 const inputTask= document.getElementById("task");
 const taskList = document.getElementById("taskList");
+const taskCounter = document.getElementById("counter");
+const completeAll = document.getElementById("complete-all")
 
 const tasks = [];
+
+//function to show add button
+inputTask.addEventListener('focus', function(){
+    addBtn.style.display = "block";
+})
 
 // EventListener on add button
 addBtn.addEventListener('click', handleAddButton );
@@ -15,18 +22,34 @@ document.addEventListener('keydown', function(event){
     }
 })
 
+//function to complete all tasks
+completeAll.addEventListener('click',function(){
+    const complete = true;
+    for(let i=0;i<tasks.length;i++){
+        tasks[i].completed= complete;
+    };
+    renderList();
+})
+
+//Function to hide the add button
+document.addEventListener('click', function(event){
+    if(event.target !== inputTask){
+        addBtn.style.display = "none";
+    }
+})
+
 // Function to create an object when button is clicked
 function handleAddButton(){
-    const text = inputTask.value;
-    if(text !== null)
+    const text = inputTask.value.trim();
+    if(text !== "")
     {
         const task = {
             title: text,
             id: Date.now(),
-            completed: false
+            completed: false,
         }
         // console.log(task);
-        inputTask.value = " ";
+        inputTask.value ="";
         addTask(task);
     }
 }
@@ -44,17 +67,23 @@ function addTask(task){
 
 function renderList(){
     taskList.innerHTML=" ";
+    let unCompletedTask= 0;
     for(let i=0;i<tasks.length;i++){
         addTaskToDOM(tasks[i]);
+        if(!tasks[i].completed){
+            unCompletedTask++;
+        }
+        taskCounter.textContent = unCompletedTask;
     }
 }
 
 function addTaskToDOM(task){
     const li= document.createElement('li');
     li.innerHTML= `<div>
-        <input type="checkbox">
-    <span>${task.title}</span>
-    <img src="images/close.png"></div>`;
+        <input id="${task.id}" type="checkbox" ${task.completed ? 'checked': ''}>
+    <span>${task.title}</span></div>
+    <img src="images/close.png" id="close">`;
+    console.log(task.completed);
     taskList.appendChild(li);
-    inputTask.value = " ";
+    inputTask.value = "";
 }
