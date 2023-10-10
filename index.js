0685f93b4e4d06676b0c1a60748781c53e184755
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-10-10 15:50:07 +0530

    Added task to DOM

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addBtn = document.getElementById("add-btn");
 const inputTask= document.getElementById("task");
+const taskList = document.getElementById("taskList");
 
 const tasks = [];
 
@@ -36,5 +37,24 @@ function addTask(task){
     {
         tasks.push(task);
         console.log(tasks);
+        renderList();
+        return;
     }
+}
+
+function renderList(){
+    taskList.innerHTML=" ";
+    for(let i=0;i<tasks.length;i++){
+        addTaskToDOM(tasks[i]);
+    }
+}
+
+function addTaskToDOM(task){
+    const li= document.createElement('li');
+    li.innerHTML= `<div>
+        <input type="checkbox">
+    <span>${task.title}</span>
+    <img src="images/close.png"></div>`;
+    taskList.appendChild(li);
+    inputTask.value = " ";
 }
